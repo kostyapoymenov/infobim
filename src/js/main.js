e57954e52da95476e7f3c4469fd9e531b81c5def
kostyapoymenov <kostya6699@mail.ru>
2018-03-30 17:11:34 +0300

    adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
         nav:true
     });
 });
+
+const   hamburgerBtn = document.querySelector('.hamburger'),
+        hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerBtn.addEventListener('click', function(){
+    this.classList.toggle('hamburger--active');
+    if(hamburgerMenu.classList.contains('hamburger-menu--active')){
+        document.body.style.overflow = '';
+    } else {
+        document.body.style.overflow = 'hidden';
+    }
+    hamburgerMenu.classList.toggle('hamburger-menu--active');
+});
