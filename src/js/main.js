8ca57409171dd83df0325041110402a92cbf4bf9
kostyapoymenov <kostya6699@mail.ru>
2018-04-03 01:37:15 +0300

    add section and slider portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
-        loop:true,
-        nav:true
+        loop:true
     });
 });
 
-const   hamburgerBtn = document.querySelector('.hamburger'),
-        hamburgerMenu = document.querySelector('.hamburger-menu');
+function burger(){
+    const   hamburgerBtn = document.querySelector('.hamburger'),
+            hamburgerMenu = document.querySelector('.hamburger-menu');
 
-hamburgerBtn.addEventListener('click', function(){
-    this.classList.toggle('hamburger--active');
-    if(hamburgerMenu.classList.contains('hamburger-menu--active')){
-        document.body.style.overflow = '';
-    } else {
-        document.body.style.overflow = 'hidden';
-    }
-    hamburgerMenu.classList.toggle('hamburger-menu--active');
-});
+    hamburgerBtn.addEventListener('click', function(){
+        this.classList.toggle('hamburger--active');
+        hamburgerMenu.classList.toggle('hamburger-menu--active');
+
+        if(hamburgerMenu.classList.contains('hamburger-menu--active')){
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+    });
+}
+burger();
