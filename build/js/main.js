e5b5ccc7bf126993c3ffae3b194f2f6b77dbc924
kostyapoymenov <kostya6699@mail.ru>
2018-04-21 01:38:29 +0300

    Переписал модалки на jquery

--- build/js/main.js
+++ build/js/main.js
@@ -1,141 +1,187 @@
-function Sliders() {
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel({
-            items: 1,
-            loop:true
+$(document).ready(function() {
+	function sliders() {
+		$(document).ready(function(){
+			$(".owl-carousel").owlCarousel({
+				items: 1,
+				loop:true
+			});
+		});
+	}
+
+	function burger(){
+		const   hamburgerBtn = document.querySelector('.hamburger'),
+				hamburgerItem = document.querySelectorAll('.hamburger-menu__link'),
+				hamburgerMenu = document.querySelector('.hamburger-menu');
+
+
+		hamburgerBtn.addEventListener('click', function(){
+			hamburgerBtn.classList.toggle('hamburger--active');
+			hamburgerMenu.classList.toggle('hamburger-menu--active');
+			if(hamburgerBtn.classList.contains('hamburger--active')){
+				document.body.classList.add('body-hidden');
+			} else {
+				document.body.classList.remove('body-hidden');
+			}
+		});
+
+		hamburgerMenu.addEventListener('click', function(event){
+			var target = event.target;
+
+			for(var i = 0; i < hamburgerItem.length; i++){
+				if(target == hamburgerItem[i]){
+					hamburgerBtn.classList.remove('hamburger--active');
+					hamburgerMenu.classList.remove('hamburger-menu--active');
+					document.body.classList.remove('body-hidden');
+					scroll(hamburgerItem[i]);
+				}
+			}
+
+		});
+	}
+
+	function modal(content, btn){
+        $(document).on('click', btn, function(){
+            event.preventDefault();
+
+        	var template = $(content).html();
+
+        	$('body').append("<div class='modal__overlay'></div>");
+            $('body').css("overflow", "hidden");
+			$('.modal__overlay').append(template);
+
+			$(document).on('click', '.modal__close', function(){
+                event.preventDefault();
+
+				$('.modal__overlay').remove();
+				$('.modal').remove();
+                $('body').css("overflow", "auto");
+			});
         });
-    });
-}
-Sliders();
 
-function burger(){
-    const   hamburgerBtn = document.querySelector('.hamburger'),
-            hamburgerItem = document.querySelectorAll('.hamburger-menu__link'),
-            hamburgerMenu = document.querySelector('.hamburger-menu');
+        $(document).on('click', '.btn-modal', function(){
+            event.preventDefault();
+
+        	$('.modal__form').addClass('modal__access');
+            setTimeout(function(){
+                $('.modal__overlay').remove();
+                $('.modal').remove();
+                $('body').css("overflow", "auto");
+            }, 1000);
+		});
+	}
+
+    modal('#modalOrderTemplate', '.btn__order');
+    modal('#modalFormTemplate', '.btn__consultation');
+    modal('#modalProgrammTemplate', '.course-item__btn');
+    modal('#modalServiceTemplate', '.btn__service');
+	modal('#modalAccessTemplate', '.btn__access');
 
 
-    hamburgerBtn.addEventListener('click', function(){
-        hamburgerBtn.classList.toggle('hamburger--active');
-        hamburgerMenu.classList.toggle('hamburger-menu--active');
-        if(hamburgerBtn.classList.contains('hamburger--active')){
-            document.body.classList.add('body-hidden');
-        } else {
-            document.body.classList.remove('body-hidden');
-        }
-    });
 
-    hamburgerMenu.addEventListener('click', function(event){
-        var target = event.target;
+	function scroll(){
+		var linkNav = document.querySelectorAll('[href^="#"]'),
+			V = 0;
+		for (var i = 0; i < linkNav.length; i++) {
+			linkNav[i].addEventListener('click', function(e) {
+				e.preventDefault();
+				var w = window.pageYOffset,
+					hash = this.href.replace(/[^#]*(.*)/, '$1');
+					t = document.querySelector(hash).getBoundingClientRect().top,
+					start = null;
+				requestAnimationFrame(step);
+				function step(time) {
+					if (start === null) start = time;
+					var progress = time - start,
+						r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+					window.scrollTo(0,r);
+					if (r != w + t) {
+						requestAnimationFrame(step)
+					} else {
+						location.hash = hash  
+					}
+				}
+			}, false);
+		}
+	}
 
-        for(var i = 0; i < hamburgerItem.length; i++){
-            if(target == hamburgerItem[i]){
-                hamburgerBtn.classList.remove('hamburger--active');
-                hamburgerMenu.classList.remove('hamburger-menu--active');
-                document.body.classList.remove('body-hidden');
-                scroll(hamburgerItem[i]);
-            }
-        }
+	function gallary(){
+		const   slideItem   = document.querySelectorAll('.work__slide');
 
-    });
-}
-burger();
+		for(var i = 0; i < slideItem.length; i++){
+			const   item        = slideItem[i].querySelectorAll('.work__item'),
+					workIcon    = slideItem[i].querySelectorAll('.work__icon'),
+					mainImage   = slideItem[i].querySelector('.work__img');
 
-function modal(content, btn){
+			for(var j = 0; j < item.length; j++){
 
-    const orderBtn = document.querySelectorAll(btn);
+				for(var z = 0; z < workIcon.length; z++){
 
-    function openOverlay(content){
-        const overlayElement = document.createElement('div');
-        overlayElement.classList.add('modal__overlay');
+					workIcon[z].addEventListener('click', function(e){
+						const   src     = this.getAttribute('src');
 
-        const template = document.querySelector(content);
-        overlayElement.innerHTML = template.innerHTML;
+						for(let i = 0; i < item.length; i++){
+							item[i].classList.remove('active');
+						}
+						this.parentNode.classList.add('active');
+						mainImage.setAttribute('src', src);
+					});
+				}
+			}
+		}
+	}
 
-        const closeElement = overlayElement.querySelector('.modal__close');
-        closeElement.addEventListener('click', function() {
-            document.body.removeChild(overlayElement);
-            document.body.style.overflow = '';
-        });
+	$(document).on('click','input[name=form-checker]',function(){
+		event.preventDefault();
+	});
 
-        const btnAll = overlayElement.querySelectorAll('.btn-modal');
-        for( let i = 0; i < btnAll.length; i++ ){
-           btnAll[i].addEventListener('click', function() {
-               const modalForm = overlayElement.querySelector('.modal__form');
-               modalForm.classList.add('modal__access');
-               setTimeout(function(){
-                   document.body.removeChild(overlayElement);
-                   document.body.style.overflow = '';
-               }, 1000);
+	$(document).on('submit','form',function(){
+		event.preventDefault();
 
-           });
-        }
+		let error = false;
+		
+		$(this).find('input.required').each(function() {
+			if ($(this).val().length <= 0) {
+				error = true;
+				$(this).parent('.form__block').addClass('form__block-error');
+			}
+		});
+		
+		let $email = $(this).find('input[name=form-email]');
+		let $phone = $(this).find('input[name=form-tel]');
+		let phone  = $phone.val().replace(/\D/g, "");
+		
+		if (phone.length < 8) {
+			error = true;
+			$phone.parent('.form__block').addClass('form__block-error');
+		}		
+		
+		if (!/.+@.+\..+/i.test($email.val())) {
+			error = true;
+			$email.parent('.form__block').addClass('form__block-error');
+		}
 
-        return overlayElement;
-    }
+		if ($(this).find("input[name=checker]").is(":checked")) {
+			error = true;
+		}
+		
+		if (!error) {
+			let data = {
+				phone : $(this).find('input[name=form-tel]').val(),
+				email : $(this).find('input[name=form-email]').val(),
+				name  : $(this).find('input[name=form-name]').val(),
+				text  : $(this).find('textarea').val(),
+				form  : $(this).prev().html(),
+			};
+		}
+	});
 
-    const overlay = openOverlay(content);
+	$(document).on('focus','input.required',function(){
+		$(this).parent('.form__block').removeClass('form__block-error');
+	});
 
-    for( var i = 0; i < orderBtn.length; i++ ){
-        orderBtn[i].addEventListener('click',function() {
-            document.body.appendChild(overlay);
-            document.body.style.overflow = 'hidden';
-        });
-    }
-}
-modal('#modalProgrammTemplate', '.course-item__btn');
-modal('#modalFormTemplate', '.btn__consultation');
-modal('#modalOrderTemplate', '.btn__order');
+	sliders();
+	burger();
+	scroll();
+	gallary();
 
-function scroll(){
-    var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
-        V = 0;
-    for (var i = 0; i < linkNav.length; i++) {
-        linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
-            e.preventDefault(); //отменяем стандартное поведение
-            var w = window.pageYOffset,  // производим прокрутка прокрутка
-                hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
-                t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
-                start = null;
-            requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
-            function step(time) {
-                if (start === null) start = time;
-                var progress = time - start,
-                    r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-                window.scrollTo(0,r);
-                if (r != w + t) {
-                    requestAnimationFrame(step)
-                } else {
-                    location.hash = hash  // URL с хэшем
-                }
-            }
-        }, false);
-    }
-}
-scroll();
-
-function gallary(){
-    const   slideItem   = document.querySelectorAll('.work__slide');
-
-    for(var i = 0; i < slideItem.length; i++){
-        const   item        = slideItem[i].querySelectorAll('.work__item'),
-                workIcon    = slideItem[i].querySelectorAll('.work__icon'),
-                mainImage   = slideItem[i].querySelector('.work__img');
-
-        for(var j = 0; j < item.length; j++){
-
-            for(var z = 0; z < workIcon.length; z++){
-
-                workIcon[z].addEventListener('click', function(e){
-                    const   src     = this.getAttribute('src');
-
-                    for(let i = 0; i < item.length; i++){
-                        item[i].classList.remove('active');
-                    }
-                    this.parentNode.classList.add('active');
-                    mainImage.setAttribute('src', src);
-                });
-            }
-        }
-    }
-}
-gallary();
+});
